0449421696111c55acd94afda09db725cba1c69d
emmablair <e_blair@ColoradoCollege.edu>
2018-04-11 14:44:20 -0600

    committing from emma's

--- src/Tournament.java
+++ src/Tournament.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 
 /**
  * @author Nicole
@@ -12,7 +10,7 @@ public class Tournament {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hi");
 
 	}
 
