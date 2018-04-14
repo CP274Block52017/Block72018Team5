806d0f848598a8c75308b222d9393883e37abdca
emmablair <e_blair@ColoradoCollege.edu>
2018-04-14 12:47:16 -0600

    Updated database to display all information for athletes and Javadoc

--- src/PlayerDatabase.java
+++ src/PlayerDatabase.java
@@ -5,35 +5,36 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.sql.ResultSet;
 
+/**
+ * This class will create a database of athletes/players and their 
+ * statistics to be used by the tournament strategies to determine
+ * the winner of the tournament. Specific statistics and players
+ * can be altered by any potential administrator of the game to fit
+ * their desired tournament. 
+ * For this project, Colorado College athletes were used. Their statistics and 
+ * information was gathered from the Colorado College Athletics website.
+ * @author emmablair
+ *
+ */
 public class PlayerDatabase {
 	
-	/**
-	 * For the first iteration, we will only use names in order to test strategies
-	 * before the database is completely set up.
-	 * int height, int age, int gamePlayed, int minutes, int wins, int losses
-	 * Possibles attributes for the players / athletes
-	 * Basketball
-	 * Name, Height, Age, # of Games Played, Wins, Losses
-	 * Hockey 
-	 * Name, Height, Age, # of Games Played, Wins, Losses
-	 * Soccer
-	 * Name, Height, Age, # of Games Played, Wins, Losses
-	 * Lacrosse
-	 * Name, Height, Age, # of Games Played, Wins, Losses
-	 * Tennis ??? 
-	 * Name, Height, Age, Matches Played, Wins, Losses
-	 * Volleyball
-	 * Name, Height, Age, # of Games PLayed, Wins, Losses
-	 */
-	
 	public static final String PORT_NUMBER = "8889";
-	
 	private static ArrayList<Players> players = new ArrayList<Players>();
 	
+	/**
+	 * Constructor is empty.
+	 */
 	public PlayerDatabase() {
 		
 	}
 	
+	/**
+	 * 
+	 * Creates the database and table of athletes and 
+	 * their information. This information will be used by the 
+	 * tournament strategies.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		try (
 			Connection conn = DriverManager.getConnection(
@@ -95,7 +96,12 @@ public class PlayerDatabase {
 				String sport = allPlayers.getString("Sport");
 				String name = allPlayers.getString("Name");
 				int height = allPlayers.getInt("Height");
-				System.out.println(sport + ", " + name + ", " + height);;
+				int gamesPlayed = allPlayers.getInt("GamesPlayed");
+				int wins = allPlayers.getInt("Wins");
+				int losses = allPlayers.getInt("Losses");
+				int year = allPlayers.getInt("Year");
+				System.out.println("Sport: " + sport + ", Name: " + name + ", Height(in): " + height + ", Games played: " + gamesPlayed + 
+						", Total wins: " +  wins + ", Total losses: " + losses + ", Year in school: " + year);;
 				rowCount++;
 			}
 			System.out.println("Total number of records = " + rowCount);
@@ -106,6 +112,10 @@ public class PlayerDatabase {
 		}
 	}
 	
+	/**
+	 * Gets the players entered into the database.
+	 * @return
+	 */
 	public static ArrayList<Players> getPlayers() {
 		System.out.println(players);
 		return players;
