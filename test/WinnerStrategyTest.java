0b26704fa69616d69afa9fd2df474b5416d60b94
emmablair <e_blair@ColoradoCollege.edu>
2018-04-15 00:27:15 -0600

    tests for all new strategies, will need to adjust a little

--- test/WinnerStrategyTest.java
+++ test/WinnerStrategyTest.java
@@ -19,10 +19,12 @@ public class WinnerStrategyTest {
 	
 	private TournamentWinnerStrategy testStrategy;
 	private ArrayList<Team> testTeams;
+	private ArrayList<Team> testAllStrategiesTeams;
+	Team test1;
+	Team test2;
 
 	@Before
 	public void setUp() {
-		testStrategy = new FirstTeamWinsStrategy();
 		testTeams = new ArrayList<Team>();
 		Team testTeam1 = new Team("Test Team 1");
 		Team testTeam2 = new Team("Test Team 2");
@@ -36,36 +38,99 @@ public class WinnerStrategyTest {
 			testTeam4.addPlayer(new Players("Janet", "Golf", "Female", 55, 3, 7, 2, 4));
 			testTeam5.addPlayer(new Players("Dan", "Tennis", "Male", 78, 9, 11, 6, 4));
 		}
+		
 		testTeams.add(testTeam1);
 		testTeams.add(testTeam2);
 		testTeams.add(testTeam3);
 		testTeams.add(testTeam4);
 		testTeams.add(testTeam5);
 		Collections.shuffle(testTeams);
+		
+		testAllStrategiesTeams = new ArrayList<Team>();
+		test1 = new Team("Test 1");
+		test1.addPlayer(new Players("Emma", "Basketball", "Female", 5, 6, 10, 5, 4));
+		test1.addPlayer(new Players("Kelli", "Soccer", "Female", 7, 8, 12, 7, 3));
+		test1.addPlayer(new Players("Nicole", "Lacrosse", "Female", 6, 7, 10, 5, 4));
+		test1.addPlayer(new Players("Janet", "Golf", "Female", 7, 8, 12, 7, 3));
+		test1.addPlayer(new Players("Dan", "Tennis", "Male", 5, 6, 10, 5, 4));
+		
+		test2 = new Team("Test 2");
+		test2.addPlayer(new Players("Emma", "Basketball", "Female", 3, 4, 8, 1, 2));
+		test2.addPlayer(new Players("Kelli", "Soccer", "Female", 5, 6, 10, 3, 3));
+		test2.addPlayer(new Players("Nicole", "Lacrosse", "Female", 3, 4, 8, 1, 2));
+		test2.addPlayer(new Players("Janet", "Golf", "Female", 5, 6, 10, 3, 3));
+		test2.addPlayer(new Players("Dan", "Tennis", "Male", 3, 4, 8, 1, 2));
+		
 	}
 	
+	//First Team Wins
 	@Test
 	public void testsFirstTwoTeams() {
+		testStrategy = new FirstTeamWinsStrategy();
 		Team winningTestTeam = testStrategy.determineWinner(testTeams.get(0), testTeams.get(1));
 		assertEquals(testTeams.get(0), winningTestTeam);
 	}
 	
 	@Test
 	public void testsLastTwoTeams() {
+		testStrategy = new FirstTeamWinsStrategy();
 		Team winningTestTeam = testStrategy.determineWinner(testTeams.get(2), testTeams.get(3));
 		assertEquals(testTeams.get(2), winningTestTeam);
 	}
 	
 	@Test
 	public void testsFirstandLastTeams() {
+		testStrategy = new FirstTeamWinsStrategy();
 		Team winningTestTeam = testStrategy.determineWinner(testTeams.get(0), testTeams.get(3));
 		assertEquals(testTeams.get(0), winningTestTeam);
 	}
 	
 	@Test
 	public void testsMiddleTwoTeams() {
+		testStrategy = new FirstTeamWinsStrategy();
 		Team winningTestTeam = testStrategy.determineWinner(testTeams.get(1), testTeams.get(2));
 		assertEquals(testTeams.get(1), winningTestTeam);
+	}
+	
+	
+	//ByClassYearStrategy
+	@Test
+	public void testClassYearStrategy() {
+		testStrategy = new ByClassYearStrategy();
+		Team winningTestTeam = testStrategy.determineWinner(test1, test2);
+		assertEquals(test1, winningTestTeam);
+	}
+	
+	//ByGamesLostStrategy
+	@Test
+	public void testGamesLostStrategy() {
+		testStrategy = new ByGamesLostStrategy();
+		Team winningTestTeam = testStrategy.determineWinner(test1, test2);
+		assertEquals(test1, winningTestTeam);
+	}
+	
+	//ByGamesPlayedStrategy
+	@Test
+	public void testGamesPlayedStrategy() {
+		testStrategy = new ByGamesPlayedStrategy();
+		Team winningTestTeam = testStrategy.determineWinner(test1, test2);
+		assertEquals(test1, winningTestTeam);
+	}
+	
+	//ByGamesWonStrategy
+	@Test
+	public void testGamesWinStrategy() {
+		testStrategy = new ByGamesWonStrategy();
+		Team winningTestTeam = testStrategy.determineWinner(test1, test2);
+		assertEquals(test1, winningTestTeam);
+	}
+	
+	//ByHeightStrategy
+	@Test
+	public void testHeightStrategy() {
+		testStrategy = new ByHeightStrategy();
+		Team winningTestTeam = testStrategy.determineWinner(test1, test2);
+		assertEquals(test1, winningTestTeam);
 	}
 
 }
