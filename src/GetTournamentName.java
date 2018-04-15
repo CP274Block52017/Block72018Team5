55b51f17b873665af102f795d0bf30a137f425a5
kellisullivan <k_sullivan@coloradocollege.edu>
2018-04-15 09:09:04 -0600

    Updates to tournament maker GUI

--- src/GetTournamentName.java
+++ src/GetTournamentName.java
@@ -4,10 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 
 
 public class GetTournamentName extends JFrame{
@@ -17,34 +19,61 @@ public class GetTournamentName extends JFrame{
 	private JTextField nameTextField;
 	private JButton continueButton;
 	private JLabel label;
-	private ActionListener nameListener;
+	private ActionListener buttonListener;
+	private ActionListener textListener;
+	private ActionListener choiceListener;
 	private String prompt;
 	private boolean hasBeenClicked;
+	private JComboBox participantsComboBox;
 	
 	private static final int START_FRAME_WIDTH = 500;
 	private static final int START_FRAME_HEIGHT = 500;
 	
-	private static final int START_FIELD_WIDTH = 10;
+	private static final int TEXT_FIELD_WIDTH = 10;
 	
 	
 	public GetTournamentName(String prompt){
 		this.prompt = prompt;
 		
-		createTextField();
-		createButton();
+		choiceListener = new ChoiceListener();
+		textListener = new TextListener();
+		buttonListener = new ContinueListener();
+		
+		JPanel comboPanel = createComboBox();
+		JPanel namePanel = createTournamentTextField();
+		createContinueButton();
 		createPanel();
 		
 		setSize(START_FRAME_WIDTH, START_FRAME_HEIGHT);	
 	}
 	
-	public void createTextField() {
-		startLabel = new JLabel("Who is creating a tournament today?");
-		nameTextField = new JTextField(START_FIELD_WIDTH);
+
+	public JPanel createComboBox() {
+		startLabel = new JLabel("How many participants do you want?");
+		participantsComboBox = new JComboBox();
+		participantsComboBox.addItem(2);
+		participantsComboBox.addItem(4);
+		participantsComboBox.addItem(8);
+		participantsComboBox.addItem(16);
+		participantsComboBox.addActionListener(choiceListener);
+		JPanel panel = new JPanel();
+		panel.add(startLabel);
+		panel.add(participantsComboBox);
+		return panel;
 	}
-	public void createButton() {
+	
+	public JPanel createTournamentTextField() {
+		startLabel = new JLabel("What is the name of the tournament?");
+		nameTextField = new JTextField(TEXT_FIELD_WIDTH);
+		JPanel panel = new JPanel();
+		panel.add(startLabel);
+		panel.add(nameTextField);
+		return panel;
+	}
+	public void createContinueButton() {
 		continueButton = new JButton("Next");
-		nameListener = new StartButton();
-		continueButton.addActionListener(nameListener);
+		buttonListener = new ContinueListener();
+		continueButton.addActionListener(buttonListener);
 	}
 
 	public boolean getClick() {
@@ -61,20 +90,40 @@ public class GetTournamentName extends JFrame{
 		add(panel);
 	}
 
-class StartButton extends JFrame implements ActionListener {
+class ContinueListener extends JFrame implements ActionListener {
 		
 		
-		public StartButton() {
+		public ContinueListener() {
 			hasBeenClicked = false;
 		}
 
 		public void actionPerformed(ActionEvent e) {
-			JFrame frame = new ParticipantChooser();
-			frame.setBackground(Color.black);
-			frame.setTitle("Start Menu");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setVisible(true);
+			System.out.println("Continue button pressed");
 		}
 	}
+
+
+class TextListener extends JFrame implements ActionListener{
+	
+	public TextListener() {
+		hasBeenClicked = false;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		System.out.println();
+	}
+}
+
+class ChoiceListener extends JFrame implements ActionListener {
+	
+	
+	public ChoiceListener() {
+		hasBeenClicked = false;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("Choice picked");
+	}
+}
 	
 }
