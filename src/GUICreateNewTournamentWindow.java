f8dbe6a283de95d9c1a201bf7b3938c6af5714ac
kellisullivan <k_sullivan@coloradocollege.edu>
2018-04-17 17:42:56 -0600

    GUIs with images

--- src/GUICreateNewTournamentWindow.java
+++ src/GUICreateNewTournamentWindow.java
@@ -2,14 +2,21 @@ import java.awt.EventQueue;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Choice;
+import java.awt.Font;
+
 
 public class GUICreateNewTournamentWindow {
 
@@ -25,86 +32,112 @@ public class GUICreateNewTournamentWindow {
 	private static JComboBox combobox;
 	
 	private static JTextField textField;
-	private static Choice strategyChoice;
-	private static Choice participantsChoice;
+	private static Choice choice;
+	private static Choice choice_1;
 	
 
 
 	/**
 	 * Create the application.
+	 * @throws IOException 
 	 */
-	public GUICreateNewTournamentWindow() {
+	public GUICreateNewTournamentWindow() throws IOException {
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
+	 * @throws IOException 
 	 */
-	private void initialize() {
+	private void initialize() throws IOException {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.BLACK);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblCreateANew = new JLabel("Create a New Tournament!");
+		lblCreateANew.setFont(new Font("Lucida Grande", Font.PLAIN, 45));
 		lblCreateANew.setForeground(Color.ORANGE);
-		lblCreateANew.setBounds(335, 55, 262, 16);
+		lblCreateANew.setBounds(443, 95, 595, 44);
 		frame.getContentPane().add(lblCreateANew);
 		
 		JLabel lblWhatWouldYou = new JLabel("What would you like to name your tournament?");
+		lblWhatWouldYou.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblWhatWouldYou.setForeground(Color.ORANGE);
 		lblWhatWouldYou.setBackground(Color.ORANGE);
-		lblWhatWouldYou.setBounds(279, 139, 318, 16);
+		lblWhatWouldYou.setBounds(329, 198, 798, 36);
 		frame.getContentPane().add(lblWhatWouldYou);
 		
 		textField = new JTextField();
-		textField.setBounds(372, 188, 130, 26);
+		textField.setBounds(619, 270, 224, 51);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblHowManyParticipants = new JLabel("How many participants do you want in the tournament?");
+		lblHowManyParticipants.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblHowManyParticipants.setForeground(Color.ORANGE);
-		lblHowManyParticipants.setBounds(279, 255, 348, 16);
+		lblHowManyParticipants.setBounds(266, 345, 959, 51);
 		frame.getContentPane().add(lblHowManyParticipants);
 		
 		JLabel lblWhatStrategyDo = new JLabel("What strategy do you want to use to determine the winners?");
+		lblWhatStrategyDo.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblWhatStrategyDo.setForeground(Color.ORANGE);
-		lblWhatStrategyDo.setBounds(265, 358, 384, 16);
+		lblWhatStrategyDo.setBounds(238, 485, 1027, 44);
 		frame.getContentPane().add(lblWhatStrategyDo);
 		
 		JLabel lblIfTheInformation = new JLabel("If the information above is correct, click submit below.");
+		lblIfTheInformation.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
 		lblIfTheInformation.setForeground(Color.ORANGE);
-		lblIfTheInformation.setBounds(279, 516, 348, 16);
+		lblIfTheInformation.setBounds(280, 626, 931, 36);
 		frame.getContentPane().add(lblIfTheInformation);
 		
 		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		submitListener = new SubmitButton();
 		btnSubmit.addActionListener(submitListener);
-		btnSubmit.setBounds(395, 585, 117, 29);
+		btnSubmit.setBounds(658, 692, 144, 51);
 		frame.getContentPane().add(btnSubmit);
 		
-		strategyChoice = new Choice();
-		strategyChoice.setForeground(Color.BLACK);
-		strategyChoice.setBackground(Color.ORANGE);
-		strategyChoice.add("1: Random Winner");
-		strategyChoice.add("2: By Greatest Average Height");
-		strategyChoice.add("3: By Greatest Average Games Played");
-		strategyChoice.add("4: By Greatest Average Games Won");
-		strategyChoice.add("5: By Greatest Average Games Lost");
-		strategyChoice.add("6: By Greatest Average Class Year");
-		strategyChoice.setBounds(415, 422, 100, 500);
-		frame.getContentPane().add(strategyChoice);
+		choice = new Choice();
+		choice.setForeground(Color.BLACK);
+		choice.setBackground(Color.ORANGE);
+		choice.add("1: Random Winner");
+		choice.add("2: By Greatest Average Height");
+		choice.add("3: By Greatest Average Games Played");
+		choice.add("4: By Greatest Average Games Won");
+		choice.add("5: By Greatest Average Games Lost");
+		choice.add("6: By Greatest Average Class Year");
+		choice.setBounds(619, 561, 216, 36);
+		frame.getContentPane().add(choice);
 		
-		participantsChoice = new Choice();
-		participantsChoice.setForeground(Color.BLACK);
-		participantsChoice.setBackground(Color.ORANGE);
-		participantsChoice.add("2");
-		participantsChoice.add("4");
-		participantsChoice.add("8");
-		participantsChoice.add("16");
-		participantsChoice.setBounds(425, 296, 100, 100);
-		frame.getContentPane().add(participantsChoice);
+		choice_1 = new Choice();
+		choice_1.setForeground(Color.BLACK);
+		choice_1.setBackground(Color.ORANGE);
+		choice_1.add("2");
+		choice_1.add("4");
+		choice_1.add("8");
+		choice_1.add("16");
+		choice_1.setBounds(619, 424, 224, 36);
+		frame.getContentPane().add(choice_1);
+		
+		String logoFile = "Logo.jpg";
+	    File logo_file = new File(logoFile);
+	    BufferedImage CC_logo = ImageIO.read(logo_file);
+	    JLabel graphic = new JLabel(new ImageIcon(CC_logo));
+	    graphic.setBounds(25, 155, 300, 1200);
+	    frame.getContentPane().add(graphic);
+	    
+	    
+	    String topBar = "TopLine.jpg";
+	    File topbar_file = new File(topBar);
+	    BufferedImage top_bar = ImageIO.read(topbar_file);
+	    JLabel top_graphic = new JLabel(new ImageIcon(top_bar));
+	    top_graphic.setBounds(100, 0, 1200, 100);
+	    frame.getContentPane().add(top_graphic);
+	    
+	    
 		frame.setBounds(100, 100, FRAME_WIDTH, FRAME_HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 	
 	
@@ -137,14 +170,16 @@ public class GUICreateNewTournamentWindow {
 				public void run() {
 					try {
 						tournamentName = textField.getText();
-						participantNumber = participantsChoice.getSelectedItem();
-						//participantNumber = Integer.parseInt(participantsChoice.getSelectedItem());
-						//strategyNumber = Character.getNumericValue((strategyChoice.getSelectedItem().charAt(0)));
-						
-						String text = textField.getText();
-						int item = Character.getNumericValue((strategyChoice.getSelectedItem().charAt(0)));
-						int item1 = Integer.parseInt(participantsChoice.getSelectedItem());
-						System.out.println(text+ " " + item + " " + item1);
+						participantNumber = choice_1.getSelectedItem();
+//						//participantNumber = Integer.parseInt(participantsChoice.getSelectedItem());
+						strategyNumber = choice.getSelectedItem();
+//						
+//						String text = textField.getText();
+//						int item = Character.getNumericValue((strategyChoice.getSelectedItem().charAt(0)));
+//						int item1 = Integer.parseInt(participantsChoice.getSelectedItem());
+//						System.out.println(text+ " " + item + " " + item1);
+						GUIShowTournamentInformation window = new GUIShowTournamentInformation();
+						window.getFrame().setVisible(true);
 						
 					} catch (Exception e) {
 						e.printStackTrace();
---
+++ src/GUIShowTournamentInformation.java
@@ -0,0 +1,185 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class GUIShowTournamentInformation {
+
+	private static JFrame frame;
+	private static final int FRAME_WIDTH = 1500;
+	private static final int FRAME_HEIGHT = 1000;
+	private ActionListener nextListener;
+	private ActionListener backListener;
+	private static boolean hasBeenClicked;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIShowTournamentInformation window = new GUIShowTournamentInformation();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 * @throws IOException 
+	 */
+	public GUIShowTournamentInformation() throws IOException {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 * @throws IOException 
+	 */
+	private void initialize() throws IOException {
+		frame = new JFrame();
+		frame.getContentPane().setForeground(Color.WHITE);
+		frame.getContentPane().setBackground(Color.BLACK);
+		frame.setBounds(100, 100, FRAME_WIDTH, FRAME_HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblTournamentInformation = new JLabel("Tournament Information!");
+		lblTournamentInformation.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
+		lblTournamentInformation.setForeground(Color.ORANGE);
+		lblTournamentInformation.setBounds(475, 63, 506, 36);
+		frame.getContentPane().add(lblTournamentInformation);
+		
+		JLabel lblTournamentName = new JLabel("Tournament Name:");
+		lblTournamentName.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
+		lblTournamentName.setForeground(Color.ORANGE);
+		lblTournamentName.setBounds(271, 176, 335, 36);
+		frame.getContentPane().add(lblTournamentName);
+		
+		JLabel lblNumberOfParticipants = new JLabel("Number of Participants:");
+		lblNumberOfParticipants.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
+		lblNumberOfParticipants.setForeground(Color.ORANGE);
+		lblNumberOfParticipants.setBounds(271, 301, 405, 43);
+		frame.getContentPane().add(lblNumberOfParticipants);
+		
+		JLabel lblStrategyToDetermine = new JLabel("Strategy to Determine Winner:");
+		lblStrategyToDetermine.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
+		lblStrategyToDetermine.setForeground(Color.ORANGE);
+		lblStrategyToDetermine.setBounds(271, 430, 515, 43);
+		frame.getContentPane().add(lblStrategyToDetermine);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
+		lblNewLabel.setText(GUICreateNewTournamentWindow.getStrategy());
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBounds(790, 430, 723, 43);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
+		lblNewLabel_1.setText(GUICreateNewTournamentWindow.getTournamentName());
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setBounds(618, 176, 768, 36);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel label = new JLabel("");
+		label.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
+		label.setText(GUICreateNewTournamentWindow.getParticipantNumber());
+		label.setForeground(Color.WHITE);
+		label.setBounds(688, 301, 603, 36);
+		frame.getContentPane().add(label);
+		
+		JLabel lblIfTheInformation = new JLabel("If the information is correct, press Next. ");
+		lblIfTheInformation.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
+		lblIfTheInformation.setForeground(Color.ORANGE);
+		lblIfTheInformation.setBounds(271, 595, 710, 36);
+		frame.getContentPane().add(lblIfTheInformation);
+		
+		JButton btnNext = new JButton("Next");
+		btnNext.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		nextListener = new NextButton();
+		btnNext.addActionListener(nextListener);
+		btnNext.setBounds(1115, 588, 154, 43);
+		frame.getContentPane().add(btnNext);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
+		backListener = new BackButton();
+		btnBack.addActionListener(backListener);
+		btnBack.setBounds(1115, 661, 154, 43);
+		frame.getContentPane().add(btnBack);
+		
+		JLabel lblIfYouWant = new JLabel("If you want to edit the information, press Back.");
+		lblIfYouWant.setForeground(Color.ORANGE);
+		lblIfYouWant.setFont(new Font("Lucida Grande", Font.PLAIN, 35));
+		lblIfYouWant.setBounds(271, 668, 793, 36);
+		frame.getContentPane().add(lblIfYouWant);
+		
+		String logoFile = "Logo.jpg";
+	    File logo_file = new File(logoFile);
+	    BufferedImage CC_logo = ImageIO.read(logo_file);
+	    JLabel graphic = new JLabel(new ImageIcon(CC_logo));
+	    graphic.setBounds(25, 155, 300, 1200);
+	    frame.getContentPane().add(graphic);
+	    
+	    
+	    String topBar = "TopLine.jpg";
+	    File topbar_file = new File(topBar);
+	    BufferedImage top_bar = ImageIO.read(topbar_file);
+	    JLabel top_graphic = new JLabel(new ImageIcon(top_bar));
+	    top_graphic.setBounds(100, 0, 1200, 100);
+	    frame.getContentPane().add(top_graphic);
+	}
+	
+	public static JFrame getFrame() {
+		return frame;
+	}
+	
+class NextButton extends JFrame implements ActionListener {
+		
+		public NextButton() {
+			hasBeenClicked = false;
+		}
+
+		public void actionPerformed(ActionEvent e) {
+
+			hasBeenClicked = true;
+		}
+	}
+	
+	class BackButton extends JFrame implements ActionListener {
+		
+		public BackButton() {
+			hasBeenClicked = false;
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						GUICreateNewTournamentWindow window = new GUICreateNewTournamentWindow();
+						window.getFrame().setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			hasBeenClicked = false;
+		}
+	}
+
+}
